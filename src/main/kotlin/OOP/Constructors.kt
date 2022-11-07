9fe7243745e2fb5d2b58ad1e2e515e812800c92a
Janet <janetmutua04@gmail.com>
2022-11-07 20:53:38 +0300

    Feat:constructors, interfaces, classes

---
+++ src/main/kotlin/OOP/Constructors.kt
@@ -0,0 +1,77 @@
+package OOP
+
+//constructors in Kotlin
+
+fun main(args:Array<String>){
+    var workerOne = Worker("Janet", 23)
+
+    var myPlant = Monstera("Monstera", 2, "Houseplant", "Clean energy")
+    println(myPlant.plantName)
+
+}
+
+//==============preferred method:=======================
+
+class People(val name:String, age: Int= 0)
+
+//============Other methods=============================
+
+class Teacher(_name:String){
+    val name = _name
+}
+
+class Staff(name:String){
+    val name:String
+    //use the init function to initialize the variable name
+    init {
+        this.name = name
+    }
+}
+
+//secondary constructors; alternative of adding a default conctructor
+
+open class Worker(name:String){
+    constructor(name: String, age: Int): this(name) //default constructors
+
+}
+
+open class WorkerTwo(name: String, age: Int=0)
+
+//derivation hierarchy of constructors
+//calling super class constructors
+
+class Being(name: String): Worker(name)
+
+class Human: Worker{
+    constructor(name: String): super(name)
+}
+
+
+//=================================================================================================================
+// implementation
+
+
+open class HousePlant(var plantName: String, var plantAge: Int){
+    fun displayName(){
+        println("$plantName, $plantAge")
+    }
+}
+
+
+class Monstera (plantName: String, plantAge: Int, _plantType:String) : HousePlant(plantName, plantAge){
+    var plantType : String
+    var plantStrength: String
+    init{
+        this.plantType = _plantType
+        this.plantStrength = ""
+    }
+    // second constructor
+    constructor(plantName: String, plantAge: Int, _plantType:String, plantStrength: String): this(plantName, plantAge, _plantType){
+        this.plantStrength = plantStrength
+    }
+
+
+
+}
+
+
---
+++ src/main/kotlin/OOP/SealedClasses.kt
@@ -0,0 +1,26 @@
+@file:JvmName("Sealed Classes")
+
+package OOP
+
+fun main(args: Array<String>){
+    testTrainAi(trainAI.visualAI())
+    testTrainAi(trainAI.timerAI())
+    testTrainAi((trainAI.deepLearning("2.0")))
+}
+
+
+sealed class trainAI{
+    class visualAI : trainAI()
+    class timerAI : trainAI()
+    class deepLearning(val timer: String): trainAI()
+}
+
+
+fun testTrainAi(version : trainAI){
+    when(version){
+        is trainAI.visualAI -> println("This is a visual AI")
+        is trainAI.timerAI -> println("This is a timer AI")
+        is trainAI.deepLearning -> println(version.timer)
+    }
+
+}
